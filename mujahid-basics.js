e0973ee4d804af78b5a8d55b72c8fde9747357d0
Mujahid420-tech <ariefdienm4@gmail.com>
2025-10-29 14:40:42 +0200

    Using js variables, string handling, numbers and type conversions.

---
+++ mujahid-basics.js
@@ -0,0 +1,7 @@
+const firstName = "Moegamad Mujahid";
+const lastName = "Ariefdien";
+let age = 21;
+const fullName = firstName + ' ' + lastName;
+console.log(" Hello, my name is " + fullName + " and I am " + age + " years of age. ");
+age = 22;
+console.log(" Next year I'll be " + age + " years of age. ");
---
+++ mujahid-operation-types.js
@@ -0,0 +1,11 @@
+const number1 = 105;
+const number2 = 210;
+const number1String = String(number1);
+const sum = number1 + number2;
+const product = number1 * number2;
+
+console.log(" number1: ", number1);
+console.log(" number2: ", number2);
+console.log(" number1 as a String: ", number1);
+console.log(" sum of number1 and number2: ", number1 + number2);
+console.log(" product of number1 and number2: ", number1 * number2);
